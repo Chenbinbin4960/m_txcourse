ca23ac4acbfaff064677dd9d3238c1474923c2fb
chenbinbin <chenbinbin@gcsoft.com>
2021-08-15 12:20:22 +0800

    修改

--- .history/src/components/HelloWorld_20210815121838.vue
+++ .history/src/components/HelloWorld_20210815121838.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
+   、
   </div>
 </template>
 
@@ -9,7 +11,7 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Hello World'
+      msg: 'Welcome to Your Vue.js App'
     }
   }
 }
@@ -17,7 +19,18 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1 {
+h1, h2 {
   font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
 }
 </style>
--- .history/src/components/HelloWorld_20210815121842.vue
+++ .history/src/components/HelloWorld_20210815121842.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <h2>Essential Links</h2>
   </div>
 </template>
 
@@ -9,7 +10,7 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Hello World'
+      msg: 'Welcome to Your Vue.js App'
     }
   }
 }
@@ -17,7 +18,18 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1 {
+h1, h2 {
   font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
 }
 </style>
